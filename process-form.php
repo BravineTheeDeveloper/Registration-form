d6c09eb15fc7b2642c96618f66dc7ce5f6c4d12d
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-07-14 07:18:56 +0300

    Php form

--- process-form.php
+++ process-form.php
@@ -1,5 +1,5 @@
 <?php
   if ($_POST) {
-    echo "Your name is " . $_POST['name'] . "second name is " . $_POST['secondName'] "email is " . $_POST['age'] . " Passwrod is" . $_POST['email'];
+    echo "Your name is " . $_POST['name'] . "second name is " . $_POST['secondName'] "email is " . $_POST['age'] . " Password is" . $_POST['password'];
   }
 ?>
